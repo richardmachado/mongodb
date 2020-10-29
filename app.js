aa34c8723cbe44cf0e273026e2aabdeae9dc2509
Richard Machado <mmachado.richard@gmail.com>
2020-10-29 13:31:31 -0700

    added port

--- app.js
+++ app.js
@@ -29,4 +29,8 @@ mongoose.connect(
 
 // listening
 
-app.listen(3000);
+
+const PORT = process.env.PORT || 3301;
+server.listen(PORT, () => {
+  console.log(`\n=== Server listening on port ${PORT} ===\n`);
+});
