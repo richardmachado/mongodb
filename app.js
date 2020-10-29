1662b27780ce0de7ad3fd8aa810618616e6717f0
Richard Machado <mmachado.richard@gmail.com>
2020-10-29 11:51:33 -0700

    completed mongodb tutorial

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
-const express = require('express');
-
+const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv/config");
+//middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+//import Routes
+const postRoute = require("./routes/posts");
+
+app.use("/posts", postRoute);
 
 //Routes
-app.get('/', (req, res) => {
-   res.send('we are on the app') 
+app.get("/", (req, res) => {
+  res.send("we are on the app");
 });
 
-app.get('/posts', (req, res) => {
-    res.send('we are on the posts') 
- });
+// connect to db
+mongoose.connect(
+  process.env.DB_CONNECTION,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("connected to db");
+  }
+);
 
 // listening
 
